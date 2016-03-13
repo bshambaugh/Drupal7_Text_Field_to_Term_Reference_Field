ee006d330870a153d3ed000f46c5444dc2e49411
bshambaugh <brent.shambaugh@gmail.com>
2016-03-13 10:06:33 -0400

    Added a modification to clear Drupal's field cache

--- taxonomy-term-create.drush.php
+++ taxonomy-term-create.drush.php
@@ -2,3 +2,5 @@
   require_once('taxonomy-combine-maps.drush.php');
   require_once('taxonomy-write-maps.drush.php');
   require_once('taxonomy-add-to-index.drush.php');
+  f// clear that field caches
+  field_cache_clear(TRUE);
